b7609fb9f939ba77074092f9b049fd14233de4d3
Alberto <albertobravolopez@gmail.com>
2022-11-06 14:12:09 +0100

    Comentarios ej6

--- boletin3/ej6_2.py
+++ boletin3/ej6_2.py
@@ -2,7 +2,8 @@ import sys
 
 cadeas = []
 cadea_leida = ''
-lonx_cadea_pequena = sys.maxsize
+#obteño o tamaño maximo dunha cadea para que calquera introducida sexa menor
+lonx_cadea_pequena = sys.maxsize 
 
 cadea_leida = input('Introduzca cadea ("fin" para rematar): ')
 while cadea_leida != 'fin':
@@ -19,12 +20,13 @@ else:
     i_caracter = 0
     mismatch = False
     while not mismatch and i_caracter < lonx_cadea_pequena:
-        #iterador sobre as posicions de cada cadea
+        #itera sobre cada caracter da cadea mentres haxa coincidencias en todas as cadeas
         for i_cadea in range(1, len(cadeas)):
             #iteramos desde a segunda cadea da lista para comparar o caracter actual co da cadea anterior na lista
             if cadeas[i_cadea][i_caracter] != cadeas[i_cadea - 1][i_caracter]:
                 mismatch = True
                 break
+            #se non hai coincidencia rompe o bucle e quedamos co indice actual que sirve para facer o slice xa que está unha posicion por diante do prefixo máximo común 
         if not mismatch:
             pos_max_comun += 1        
             i_caracter +=1
